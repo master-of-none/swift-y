dd239c71233697bae131621fc750f1fc8211510e
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-01-07 22:46:52 -0800

    Some functions 🏎️

---
+++ swift-tutorial/Functions.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func someRandomFunction(){
+    "Some letters"
+}
+someRandomFunction()
+
+func plusTwo(val:Int){
+    let newValue = val + 2
+}
+plusTwo(val: 67)
+
+func newPlusTwo(val: Int) -> Int {
+    val + 2
+}
+newPlusTwo(val: 67)
--- swift-tutorial/Variables.playground/Contents.swift
+++ swift-tutorial/Variables.playground/Contents.swift
@@ -1,5 +1,31 @@
 import Foundation
 
 let myName = "master"
-let yourName = "foo"
+let yourName = "Foo"
+
+var names = [
+    myName,
+    yourName
+]
+
+//names = ["Blah"]
+names.append("Fizz")
+names.append("Fuzz")
+
+let foo = "Foo"
+var foo2 = foo
+foo2 = "Foo 2"
+
+let moreNames = [
+    "foo", "Bar"
+]
+
+var copy = moreNames
+copy.append("baz")
+
+// This is by reference
+let oldArray = NSMutableArray(
+    array: ["Foo", "Bar"]
+)
+oldArray.add("Baz")
 
