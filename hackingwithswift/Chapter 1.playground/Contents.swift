c04cb36b5ba9c29a54e73941bc195d1e3e160293
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2024-10-23 19:39:36 -0400

    Chapter 1

---
+++ hackingwithswift/Chapter 1.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Shrikrishna"
+print(name)
+
+let managerName = "Micahel Scott"
+
+var intVal = 1
+var doubleVal = 1.03
+var booleanVal = true
+
+// Join Strings
+let first = "Hello"
+let second = " World"
+let finalWord = first + second
+let message = " The mesage is \(first)\(second)"
+
